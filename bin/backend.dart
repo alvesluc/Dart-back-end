b6d4bf9a1bafc6d4b2c05bf411a550582685836b
Lucas Alves <l.alves001@gmail.com>
2022-07-20 20:36:55 -0300

    Initial setup

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,5 +1,14 @@
-import 'package:backend/backend.dart' as backend;
+import 'dart:async';
 
-void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
+void main(List<String> arguments) async {
+  final server = await io.serve(_handler, '0.0.0.0', 8080);
+
+  print("[SERVER] Running on port ${server.port}");
+}
+
+FutureOr<Response> _handler(Request request) {
+  return Response(200, body: 'Body');
 }
