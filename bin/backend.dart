00c8f31a04cdeac770050defa8c2aaeba9dbd180
Lucas Alves <l.alves001@gmail.com>
2022-07-20 20:47:26 -0300

    First middleware

--- bin/backend.dart
+++ bin/backend.dart
@@ -4,9 +4,25 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
-  final server = await io.serve(_handler, '0.0.0.0', 8080);
+  var pipeline = Pipeline().addMiddleware(log());
+
+  final server = await io.serve(pipeline.addHandler(_handler), '0.0.0.0', 8080);
 
   print("[SERVER] Running on port ${server.port}");
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      // Before execution:
+      print('[REQUEST] ${request.url}');
+      var response = await handler(request);
+
+      // After execution:
+      print('[RESPONSE] ${response.statusCode}');
+      return response;
+    };
+  };
 }
 
 FutureOr<Response> _handler(Request request) {
