efd39e0a990dd725c1c9b963a93b9c9e39f192cb
Lucas Alves <l.alves001@gmail.com>
2022-07-23 00:43:02 -0300

    Started auth_resource implementation

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -6,6 +6,10 @@ import 'core/services/bcrypt/bcrypt_service_imp.dart';
 import 'core/services/database/postgres/postgres_database.dart';
 import 'core/services/database/remote_database.dart';
 import 'core/services/dot_env/dot_env_service.dart';
+import 'core/services/jwt/dart_jsonwebtoken/jwt_service_imp.dart';
+import 'core/services/jwt/jwt_service.dart';
+import 'core/services/request_extractor/request_extractor.dart';
+import 'modules/auth/auth_resource.dart';
 import 'modules/swagger/swagger_handler.dart';
 import 'modules/user/user_resource.dart';
 
@@ -15,6 +19,8 @@ class AppModule extends Module {
         Bind.singleton<DotEnvService>((i) => DotEnvService()),
         Bind.singleton<RemoteDatabase>((i) => PostgresDatabase(i())),
         Bind.singleton<BCryptService>((i) => BCryptServiceImp()),
+        Bind.singleton<JWTService>((i) => JWTServiceImp(i())),
+        Bind.singleton<RequestExtractor>((i) => RequestExtractor()),
       ];
 
   @override
@@ -22,5 +28,6 @@ class AppModule extends Module {
         Route.get('/', (Request request) => Response.ok('OK')),
         Route.get('/docs/**', swaggerHandler),
         Route.resource(UserResource()),
+        Route.resource(AuthResource()),
       ];
 }
--- lib/src/modules/auth/auth_resource.dart
+++ lib/src/modules/auth/auth_resource.dart
@@ -1,7 +1,14 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
+
+import '../../core/services/bcrypt/bcrypt_service.dart';
+import '../../core/services/database/remote_database.dart';
+import '../../core/services/jwt/jwt_service.dart';
+import '../../core/services/request_extractor/request_extractor.dart';
+import '../../utils/to_query_extension.dart';
 
 class AuthResource extends Resource {
   @override
@@ -12,8 +19,52 @@ class AuthResource extends Resource {
         Route.get('/auth/update_password', _checkToken),
       ];
 
-  FutureOr<Response> _login() {
-    return Response.ok('body');
+  FutureOr<Response> _login(Request request, Injector injector) async {
+    final extractor = injector.get<RequestExtractor>();
+    final bcrypt = injector.get<BCryptService>();
+    final jwt = injector.get<JWTService>();
+    final credentials = extractor.getAuthorizationBasic(request);
+
+    final database = injector.get<RemoteDatabase>();
+    final result = await database.query(
+      '''
+      SELECT id, role, password
+      FROM "User" WHERE email = @email;
+      '''
+          .toQuery(),
+      variables: {'email': credentials.email},
+    );
+
+    if (result.isEmpty) {
+      return Response.forbidden(jsonEncode(
+        {'error': 'Invalid email or password'},
+      ));
+    }
+
+    final userMap = result.map((element) => element['User']).first!;
+
+    if (!bcrypt.checkHash(credentials.password, userMap['password'])) {
+      return Response.forbidden(jsonEncode(
+        {'error': 'Invalid email or password'},
+      ));
+    }
+
+    final payload = userMap..remove('password');
+
+    return Response.ok(jsonEncode(_generateToken(payload, jwt)));
+  }
+
+  Map _generateToken(Map payload, JWTService jwt) {
+    payload['exp'] = _determineExpiration(Duration(minutes: 10));
+    final accessToken = jwt.generateToken(payload, 'accessToken');
+
+    payload['exp'] = _determineExpiration(Duration(days: 3));
+    final refreshToken = jwt.generateToken(payload, 'refreshToken');
+
+    return {
+      'accessToken': accessToken,
+      'refreshToken': refreshToken,
+    };
   }
 
   FutureOr<Response> _refreshToken() {
@@ -26,5 +77,14 @@ class AuthResource extends Resource {
 
   FutureOr<Response> _updatePassword() {
     return Response.ok('body');
+  }
+
+  int _determineExpiration(Duration duration) {
+    final expiresDate = DateTime.now().add(duration);
+    final expiresIn = Duration(
+      milliseconds: expiresDate.millisecondsSinceEpoch,
+    );
+
+    return expiresIn.inSeconds;
   }
 }
