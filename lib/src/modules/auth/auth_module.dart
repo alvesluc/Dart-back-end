a8b22b93b46f51ffb75367d8e12673e8e1831f92
Lucas Alves <l.alves001@gmail.com>
2022-07-25 19:56:17 -0300

    Updated shelf_modular

--- lib/src/modules/auth/auth_module.dart
+++ lib/src/modules/auth/auth_module.dart
@@ -1,8 +1,5 @@
 import 'package:shelf_modular/shelf_modular.dart';
 
-import '../../core/services/database/postgres/postgres_database.dart';
-import '../../core/services/database/remote_database.dart';
-import '../../core/services/dot_env/dot_env_service.dart';
 import 'datasources/auth_datasource_imp.dart';
 import 'repositories/auth_repository.dart';
 import 'resources/auth_resource.dart';
@@ -10,8 +7,6 @@ import 'resources/auth_resource.dart';
 class AuthModule extends Module {
   @override
   List<Bind<Object>> get binds => [
-        Bind.singleton<DotEnvService>((i) => DotEnvService()),
-        Bind.singleton<RemoteDatabase>((i) => PostgresDatabase(i())),
         Bind.singleton<AuthDatasource>((i) => AuthDatasourceImpl(i())),
         Bind.singleton((i) => AuthRepository(i(), i(), i())),
       ];
